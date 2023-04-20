2a503bf17558423cf69a74adc045f645a8242db3
Dennis McGeary <Boxbeagle@gmail.com>
2023-04-19 21:52:45 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "dawn-wood-40039",
   url: "https://dawn-wood-40039.botics.co",
   api: "https://dawn-wood-40039.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
